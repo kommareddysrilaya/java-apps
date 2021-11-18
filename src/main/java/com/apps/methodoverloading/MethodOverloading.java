e656e98c4f5e03c6ea6631396151ee91e2015bc1
kommareddysrilaya <ksrilaya99@gmail.com>
2021-11-18 20:22:22 +0530

    method overloading

--- src/main/java/com/apps/methodoverloading/MethodOverloading.java
+++ src/main/java/com/apps/methodoverloading/MethodOverloading.java
@@ -1,5 +1,18 @@
 package com.apps.methodoverloading;
 
 public class MethodOverloading {
+	public void add(int a, int b) {
+		System.out.println(" add integers" + (a + b));
+	
+	}
+	public void add(float a, float b) {
+		System.out.println(" add float numbers " + (a + b));
+	}
 
+	public void add(double a, double b) {
+		System.out.println(" add double numbers " + (a + b));
+	}
+	public void add(float a, int b) {
+		System.out.println(" add floatint numbers " + (a + b));
+	}
 }
---
+++ src/main/java/com/apps/methodoverloading/TestClass.java
@@ -0,0 +1,16 @@
+package com.apps.methodoverloading;
+
+public class TestClass {
+	public static void main(String[] args) {
+	  MethodOverloading m1=new MethodOverloading();
+	  m1.add (10,20);
+	  m1.add (2.3f,4.6f);
+	  m1.add (2.3,4.6);
+	  m1.add(2.7, 7);
+	  
+		
+	
+		
+	}
+
+}
